b70283ba4845468732076570f49c613b9362778f
Sebastián Reyes <sebastianreyes01@hotmail.com>
2018-10-25 18:11:40 -0500

    adelanto

--- src/main/java/com/edu/eci/arsw/parcial2/controllers/WeatherAPIController.java
+++ src/main/java/com/edu/eci/arsw/parcial2/controllers/WeatherAPIController.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping(value = "/weather")
+@CrossOrigin(origins = "*")
 public class WeatherAPIController {
 
     @Autowired
@@ -22,6 +24,7 @@ public class WeatherAPIController {
     @RequestMapping(method = RequestMethod.GET,path = "/{city}")
     public ResponseEntity<?> handlerGetResourceWeather(@PathVariable String city) {
         try {
+            System.out.println("entro");
             return new ResponseEntity<>(owms.getInfo(city), HttpStatus.ACCEPTED);
         } catch (Exception ex) {
             Logger.getLogger(WeatherAPIController.class.getName()).log(Level.SEVERE, null, ex);
--- src/main/resources/static/js/weatherAPI.js
+++ src/main/resources/static/js/weatherAPI.js
@@ -1,13 +1,31 @@
+var instance = axios.create({
+  baseURL: 'https://arsw-parcial2.herokuapp.com/weather/'
+//  headers: {'Access-Control-Allow-Origin': '*'}
+});
+
 var WeatherAPI = (function() {
 
     var getInfo = function() {
         var main = document.getElementsByTagName("main")[0];
         var input = document.getElementById("input");
-
-        var info = document.createElement("p");
-        var infoText = document.createTextNode(input.value);
-        info.appendChild(infoText);
-        main.appendChild(info);
+        console.log(input.value);
+        instance.get(input.value)
+                .then(function (response) {
+                    var info = document.createElement("p");
+                    var infoText = document.createTextNode(response);
+                    info.appendChild(infoText);
+                    main.appendChild(info);
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        
+//        var info = document.createElement("p");
+//        var infoText = document.createTextNode(input.value);
+//        info.appendChild(infoText);
+//        main.appendChild(info);
     };
 
     return {
